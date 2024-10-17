17c4c1505a64dbe5598b7df50adc70a1d5d63a30
Jean-Aime-2023 <mugabejeanaime96@gmail.com>
2024-10-17 16:58:26 +0200

    deploy to genezio

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,8 @@
 import express, { json, urlencoded } from 'express';
-import productsRotes from './routes/products/index';
-import authRoutes from './routes/auth/index'
+import productsRotes from './routes/products/index.js';
+import authRoutes from './routes/auth/index.js';
+import serverless from 'serverless-http';
+
 const port = 3000;
 const app = express();
 
@@ -14,6 +16,10 @@ app.get('/', (req, res) => {
 app.use('/products', productsRotes);
 app.use('/auth', authRoutes);
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+if (process.env.NODE_ENV === 'dev') {
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
+}
+
+export const handler = serverless(app);
