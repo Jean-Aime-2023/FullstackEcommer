70ef088d3c5b99f4e25a2b149c7cdd37850f8eea
Jean-Aime-2023 <mugabejeanaime96@gmail.com>
2024-10-24 15:02:55 +0200

    create expo app

--- api/src/db/usersSchema.ts
+++ api/src/db/usersSchema.ts
@@ -1,11 +1,13 @@
-import { integer, varchar, pgTable, text, timestamp } from 'drizzle-orm/pg-core';
+import { integer, pgTable, varchar, text } from 'drizzle-orm/pg-core';
 import { createInsertSchema } from 'drizzle-zod';
 
 export const usersTable = pgTable('users', {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
+
   email: varchar({ length: 255 }).notNull().unique(),
-  password: varchar().notNull(),
+  password: varchar({ length: 255 }).notNull(),
   role: varchar({ length: 255 }).notNull().default('user'),
+
   name: varchar({ length: 255 }),
   address: text(),
 });
@@ -14,6 +16,7 @@ export const createUserSchema = createInsertSchema(usersTable).omit({
   id: true,
   role: true,
 });
+
 export const loginSchema = createInsertSchema(usersTable).pick({
   email: true,
   password: true,
